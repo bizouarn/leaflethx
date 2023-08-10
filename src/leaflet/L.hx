680cf86fb526a2b6ac275e738d17ffa138116190
Aymeric <aymeric.bizouarn@hotmail.fr>
2023-08-10 14:17:47 +0200

    Add comments in L

--- src/leaflet/L.hx
+++ src/leaflet/L.hx
@@ -1,12 +1,23 @@
 package leaflet;
 
 import leaflet.*;
-@:native('L') extern class L{
+
+// Déclaration de l'externe pour Leaflet
+@:native('L') extern class L {
+    // Méthode pour créer une carte Leaflet
     @:overload(function(element:js.html.Element, ?options:MapOptions):Map_{})
     public static function map(elementId:String, ?options:MapOptions):Map_;
+
+    // Méthode pour créer une icône personnalisée
     public static function icon(options:IconOptions):Icon<IconOptions>;
-    public static function latLng(lat:Float, lng:Float):Dynamic;
-    @:overload(function(coords:Array<Float>,  ?options:MarkerOptions ):Marker<MarkerOptions> {})
-    public static function marker(coords:LatLng, ?options:MarkerOptions ):Marker<MarkerOptions>;
-    public static function tileLayer(tileUrl:String, options:TileLayerOptions ):TileLayer;
+
+    // Méthode pour créer un objet LatLng
+    public static function latLng(lat:Float, lng:Float):LatLng;
+
+    // Méthode pour créer un marqueur (marker) Leaflet
+    @:overload(function(coords:Array<Float>, ?options:MarkerOptions):Marker<MarkerOptions>{})
+    public static function marker(coords:LatLng, ?options:MarkerOptions):Marker<MarkerOptions>;
+
+    // Méthode pour ajouter une couche de tuiles (tile layer) à la carte
+    public static function tileLayer(tileUrl:String, options:TileLayerOptions):TileLayer;
 }
